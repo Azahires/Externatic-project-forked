b88901f4a86d8b91cc021299c4a8abee92a6a138
Romain HADDAD <romain.haddad@gmail.com>
2022-12-08 11:11:36 +0100

    coding the search bar and carry the search value on a useState, need to connect the button to a Onclick

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home/Home";
 import "reset-css";
 import Style from "./style";
+import SearchBar from "@components/SearchBar/SearchBar";
 
 function App() {
   return (
     <Style>
-      <Home />
+      <SearchBar />
     </Style>
   );
 }
