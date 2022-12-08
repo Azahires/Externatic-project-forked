ce0df70d2771cf084e70fe9ae78465e30b6607f9
hswiergiel <h.swiergiel@hotmail.fr>
2022-12-08 12:06:32 +0100

    connecting db, adding the offers card, adding the main font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import Home from "./pages/Home/Home";
 import "reset-css";
+import DisplayOffers from "@components/DisplayOffers/DisplayOffers";
 import Style from "./style";
 
 function App() {
   return (
     <Style>
-      <Home />
+      {/* <Home /> */}
+      <DisplayOffers />
     </Style>
   );
 }
