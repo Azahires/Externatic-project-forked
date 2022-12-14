f97ab23031b2d68423c80262e39f312f24faeb74
FlorianBartier <florianbartierpro@gmail.com>
2022-12-14 16:38:41 +0100

    creation of a block with a short presentation of externatic for the home page

--- frontend/src/components/PresentationHP/PresentationHP.jsx
+++ frontend/src/components/PresentationHP/PresentationHP.jsx
@@ -4,15 +4,15 @@ export default function PresentationHP() {
   return (
     <Style>
       <div className="presentationContainer">
+        <h1 className="typing-demo">Qui sommes-nous ?</h1>
         <p className="presentationText">
           <em>Externatic</em>, c’est avant tout une équipe d’experts IT qui,
           depuis 10 ans a développé un savoir-faire sur le recrutement
           informatique. Notre cabinet de recrutement s’appuie sur des méthodes
           authentiques, où l’humain est tout simplement indispensable.
         </p>
-
         <a href="/" className="presentationButton">
-          En savoir plus
+          <span>En savoir plus</span>
         </a>
       </div>
     </Style>
--- frontend/src/components/PresentationHP/style.jsx
+++ frontend/src/components/PresentationHP/style.jsx
@@ -6,14 +6,14 @@ export default styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 50px;
+    margin-top: 200px;
     margin-left: 50px;
     padding: 20px;
-    border-radius: 10px;
-    box-shadow: 4px 4px 25px black;
+    border-radius: 5px;
+    box-shadow: 3px 3px 25px black;
     border: 3px solid #c91961;
-    width: 600px;
-    height: 130px;
+    width: 615px;
+    height: 170px;
     font-family: "Jost", sans-serif;
     background-color: whitesmoke;
   }
@@ -21,27 +21,69 @@ export default styled.div`
     font-size: 1.3rem;
     text-align: center;
     padding-bottom: 1rem;
+    line-height: 1.4rem;
   }
   em {
+    font-size: 1.5rem;
     font-weight: bold;
     color: #c91961;
   }
   .presentationButton {
-    text-align: center;
-    text-decoration: none;
-    padding: 8px 20px;
-    width: 10rem;
-    border-radius: 10px;
-    background-color: #c91961;
-    color: white;
-    box-shadow: 2px 2px 2px gray;
-    font-size: 1.2rem;
+    display: block;
+    width: 12.5rem;
+    height: 2.5rem;
+    line-height: 2.5rem;
+    font-size: 1.125rem;
     font-weight: bold;
+    text-decoration: none;
+    color: #c91961;
+    border: 2px solid #c91961;
+    letter-spacing: 0.125rem;
+    text-align: center;
+    position: relative;
+    transition: all 0.35s;
+  }
+  .presentationButton span {
+    position: relative;
+    z-index: 2;
+  }
+  .presentationButton:after {
+    position: absolute;
+    content: "";
+    left: 0;
+    width: 0;
+    height: 100%;
+    background: #c91961;
+    transition: all 0.35s;
   }
   .presentationButton:hover {
-    background-color: white;
+    color: #fff;
+  }
+  .presentationButton:hover:after {
+    width: 100%;
+  }
+  .typing-demo {
     color: #c91961;
+    width: 39%;
+    animation: typing 2s steps(25), blink 0.6s step-end infinite alternate;
+    white-space: nowrap;
+    overflow: hidden;
+    border-right: 3px solid;
+    font-size: 1.5rem;
+    padding-bottom: 2rem;
     font-weight: bold;
-    border: solid #c91961 2px;
+    text-transform: uppercase;
+  }
+
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+  }
+
+  @keyframes blink {
+    50% {
+      border-color: transparent;
+    }
   }
 `;
