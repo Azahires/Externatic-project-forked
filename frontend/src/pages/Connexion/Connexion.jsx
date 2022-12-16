943f43ae35796e9a0fbfb809b5bbf9304140e5be
SOA-P <Amajoudnedim@gmail.com>
2022-12-16 11:25:04 +0100

    ajust style with form version 2

--- frontend/src/pages/Connexion/Connexion.jsx
+++ frontend/src/pages/Connexion/Connexion.jsx
@@ -1,5 +1,25 @@
+import { useForm } from "react-hook-form";
 import Style from "./style";
 
 export default function Connexion() {
-  return <Style>Connexion</Style>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => data;
+
+  return (
+    <Style onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Email"
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        {...register("Mot de passe", { required: true, max: 50, min: 8 })}
+      />
+      <input className="btn-1" type="submit" value="Se connecter" />
+    </Style>
+  );
 }
--- frontend/src/pages/Registration/Registration.jsx
+++ frontend/src/pages/Registration/Registration.jsx
@@ -1,5 +1,37 @@
+import { useForm } from "react-hook-form";
 import Style from "./style";
 
 export default function Registration() {
-  return <Style>Registration</Style>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => data;
+
+  return (
+    <Style onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="First name"
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...register("First name", { required: true, maxLength: 80 })}
+      />
+      <input
+        type="text"
+        placeholder="Last name"
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...register("Last name", { required: true, maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        {...register("Mot de passe", { required: true, max: 50, min: 8 })}
+      />
+      <input className="btn" type="submit" value="S'enregistrer" />
+    </Style>
+  );
 }
