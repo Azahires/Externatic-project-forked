aed49694ed99b21a5dfaff3c35bf60d56b31fad3
Romain HADDAD <romain.haddad@gmail.com>
2022-12-16 15:42:16 +0100

    last design version

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,28 @@
 import DisplayOffers from "@components/DisplayOffers/DisplayOffers";
+import LoginHP from "@components/LoginHP/LoginHP";
+import PresentationHP from "@components/PresentationHP/PresentationHP";
 import Partners from "../../components/SliderPartners/Partners";
 import ButtonLast from "../../components/SliderLastOffers/ButtonLast";
 import DescPartnes from "../../components/SliderPartners/DescPartnes";
 import DescOffers from "../../components/SliderLastOffers/DescOffers";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import Style from "./style";
 
 export default function Home() {
   return (
     <div>
-      <SearchBar />
-      <DescOffers />
-      <DisplayOffers />
-      <ButtonLast />
-      <DescPartnes />
-      <Partners />
+      <Style>
+        <SearchBar />
+        <div className="blockContainer">
+          <LoginHP />
+          <PresentationHP />
+        </div>
+        <DescOffers />
+        <DisplayOffers />
+        <ButtonLast />
+        <DescPartnes />
+        <Partners />
+      </Style>
     </div>
   );
 }
